cff6eb507654bb26539130ace03494bbf1d4374a
Carolina Ferreiro Belenguer <88061350+cferreirobelenguer@users.noreply.github.com>
2022-11-30 18:17:08 +0100

    test AddCategory

--- test/components/AddCategory.test.jsx
+++ test/components/AddCategory.test.jsx
@@ -24,14 +24,20 @@ describe("Pruebas del componente AddCategory",()=>{
         //screen.debug()
         expect(search.value).toBe('')
     }),
-    test("Verificar que funciona onSubmit de formulario si el input tiene valor ",()=>{
-        render(<AddCategory onNewCategory={()=>{}}/>)
+    test("Verificar que funciona onSubmit de formulario si el input tiene valor y la función onNewCategory es llamada 1 vez ",()=>{
+        //Función mercada por Mock
+        const onNewCategory=jest.fn()
+        render(<AddCategory onNewCategory={onNewCategory}/>)
         const input=screen.getByRole('textbox')
         const form= screen.getByRole('form')
         fireEvent.input(input,{target:{value:'pokemon'}})
         fireEvent.submit(form);
         expect(input.value).toBe("pokemon")
-        //screen.debug()
+        //Verificar si la función onNewCategory ha sido llamada una vez
+        expect(onNewCategory).toHaveBeenCalledTimes(1)
+        //Verificar si la función onNewCategory ha sido llamada con el valor de la caja de texto que es pokemon
+        expect(onNewCategory).toHaveBeenCalledWith('pokemon')
+        
     }),
     test("Verificar que funciona onSubmit de formulario si el input no tiene valor ",()=>{
         render(<AddCategory onNewCategory={()=>{}}/>)
@@ -40,7 +46,18 @@ describe("Pruebas del componente AddCategory",()=>{
         fireEvent.input(input,{target:{value:''}})
         fireEvent.submit(form);
         expect(input.value).toBe("")
-        screen.debug()
+        
+        
+    }),
+    test("Verificar que cuando se llama a onSubmit por form no se debe llamar a onNewCategory si el valor está vacío",()=>{
+        const onNewCategory=jest.fn()
+        render(<AddCategory onNewCategory={onNewCategory}/>)
+        const input=screen.getByRole('textbox')
+        const form=screen.getByRole('form')
+        fireEvent.input(input,{target:{value:""}})
+        fireEvent.submit(form)
+        //Verificar si la función onNewCategory no ha sido llamada
+        expect(onNewCategory).toHaveBeenCalledTimes(0)
     })
 
 
