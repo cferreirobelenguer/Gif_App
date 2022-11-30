c838010b89a236d2257510e393907bfc4b8c19a9
Carolina Ferreiro Belenguer <88061350+cferreirobelenguer@users.noreply.github.com>
2022-11-30 16:12:39 +0100

    test GitItem en ek que se verifica que title está en parrafo

--- test/components/GifItem.test.jsx
+++ test/components/GifItem.test.jsx
@@ -9,10 +9,17 @@ describe("Pruebas en <GifItem/>",()=>{
     test("Debe de hacer match con el snapshot",()=>{
         const {container}=render(<GifItem title={title} url={url}/>)
         expect(container).toMatchSnapshot();
+    }),
+    test("debe de mostrar la imagen con el URL y el ALT indicado",()=>{
+        render(<GifItem title={title} url={url}/>)
+        const{src,alt}=screen.getByRole('img')
+        expect(src).toBe(url)
+        expect(alt).toBe(title)
+        //screen.debug()
+    }),
+    test("Verificar que el title mostrado en etiqueta p es el correcto",()=>{
+        render(<GifItem title={title} url={url}/>)
+        expect(screen.getByText(title)).toBeTruthy()
     })
 })
 
-test("debe de mostrar la imagen con el URL y el ALT indicado",()=>{
-    render(<GifItem title={title} url={url}/>)
-    screen.debug()
-})
